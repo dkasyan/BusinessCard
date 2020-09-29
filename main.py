0e2c526e6097e49a1231e29630c5adeec8f9b177
Damian Kasyan <damian.kasyan@hotmail.com>
2020-09-29 22:35:48 +0200

    Fix faker bug

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
-#from faker import Faker
-#fake = Faker()
+from faker import Faker
+fake = Faker()
 
-#fake.name()
+fake.name()
+print(fake.company)
 #import Faker
 #import os 
 
 class BaseContact:
     def __init__(self, name, surname, email):
         self.name = name
-        self.surname = surname
-        self.email = email
+#        self.surname = surname
+#        self.email = email
+        self.company = fake.company
 
-class BusinesCard(BusinessContact):
-    def __init__(self, max_load, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.max_load = max_load
-        self.company_name = company_name
-        self.company_position = company_position
+#class BusinesCard(BusinessContact):
+#    def __init__(self, max_load, *args, **kwargs):
+#        super().__init__(*args, **kwargs)
+#        self.max_load = max_load
+#        self.company_name = company_name
+#        self.company_position = company_position
 
 
   #  Używając dziedziczenia, rozdziel podstawową klasę wizytówki na dwie osobne: pierwsza (BaseContact) powinna przechowywać podstawowe dane kontaktowe takie jak imię, nazwisko, telefon, adres e-mail. Za pomocą kolejnej klasy (BusinessContact) rozszerz klasę bazową o przechowywanie informacji związanych z pracą danej osoby – stanowisko, nazwa firmy, telefon służbowy.
