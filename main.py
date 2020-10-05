2da0cf6af743429ab903f1041ed0dfd0fb4f30c8
Damian Kasyan <damian.kasyan@hotmail.com>
2020-10-05 23:25:21 +0200

    Finish

--- main.py
+++ main.py
@@ -13,12 +13,12 @@ fake = Faker('pl_PL')
 
 
 #fake.profile()
-fake_profil = fake.profile()
-f_name = str.split(fake_profil["name"])[0]
-f_surname = str.split(fake_profil["name"])[1]
+fakek_profil = fake.profile()
+f_name = str.split(fakek_profil["name"])[0]
+f_surname = str.split(fakek_profil["name"])[1]
 f_phone = fake.phone_number()
-f_email = str.split(fake_profil["mail"])[0]
-f_job = str.split(fake_profil["job"])
+f_email = str.split(fakek_profil["mail"])[0]
+f_job = str.split(fakek_profil["job"])
 f_company = fake.bs()
 f_businesphone = fake.phone_number()
 
@@ -53,17 +53,42 @@ class BusinessContact(BaseContact):
     def __str__(self):
         return f'{self.name} {self.surname} {self.phone} {self.email} {self.job} {self.company} {self.businesphone}'
 
+#Losowa wizytówka
+
+types = True
+amount = 29999999
+def create_contacts (types, amount):
+    for i in range(amount):
+        fakek_profil = fake.profile()
+        fk_name = str.split(fakek_profil["name"])[0]
+        fk_surname = str.split(fakek_profil["name"])[1]
+        fk_phone = fake.phone_number()
+        fk_email = str.split(fakek_profil["mail"])[0]
+        fk_job = str.split(fakek_profil["job"])
+        fk_company = fake.bs()
+        fk_businesphone = fake.phone_number()
+        if types == True:
+            private = BaseContact(name = fk_name, surname = fk_surname, phone = fk_phone, email = fk_email)
+            private.contact()
+        else:
+            business = BusinessContact(name = fk_name, surname = fk_surname, phone = fk_phone, email = fk_email, job = fk_job, company = fk_company, businesphone = fk_businesphone)
+            business.contact()
+            
+
+
+create_contacts(True,amount)
+
 print("################")      
 kolega = BaseContact(name = f_name, surname = f_surname, phone = f_phone, email = f_email)
 kolega.contact()
 #print(kolega)
 kolega.label_length()
+
 print("################")
 wspolpracownik = BusinessContact(name = f_name, surname = f_surname, phone = f_phone, email = f_email, job = f_job, company = f_company, businesphone = f_businesphone)
 wspolpracownik.contact()
 #print(wspolpracownik)
-##### losowa wizytówka
 
-def create_contacts (type, amount):
+
 
 
