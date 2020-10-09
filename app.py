c34997678cefc22a641e076a070ca565607d6c81
Damian Kasyan <damian.kasyan@hotmail.com>
2020-10-09 18:56:23 +0200

    problem

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
 
 app = Flask(__name__)
 
-@app.route('/hello')
+@app.route('/')
 def hello():
     my_name = "Johns"
     return f'Hello, {my_name}!'
@@ -19,3 +22,25 @@ def blog(id):
 @app.route('/message', methods=['POST'])
 def post_message():
     return "OK"
+
+@app.route('/greeting', methods=['GET', 'POST'])
+def greeting():
+    if request.method == 'GET':
+        print("Wyslano GET")
+        return render_template("greeting.html")
+    elif request.form == 'POST':
+
+        print("Wysłano POST")
+        print("We received POST")
+        print(request.form)
+        return redirect("/")
+       # print(request.greeting)
+        #return redirect("/greeting")
+ #   return text
+
+
+
+#@app.route('/message', methods=['POST'])
+#def message():
+#    print(request.form)
+#    return redirect("/message")
