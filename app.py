143ea2901aca89fb7cca27b447a9c31ba2a53c01
Damian Kasyan <damian.kasyan@hotmail.com>
2020-10-09 17:25:32 +0200

    Add new routing

--- app.py
+++ app.py
@@ -2,7 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def hello():
-    my_name = "John"
+    my_name = "Johns"
     return f'Hello, {my_name}!'
+
+@app.route('/home')    
+def home():
+    my_name = "Damian"
+    return f'Hello, {my_name}!'
+
+@app.route('/blog/<id>')
+def blog(id):
+    return f"This is blog entry #{id}"
+
+@app.route('/message', methods=['POST'])
+def post_message():
+    return "OK"
