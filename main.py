fb3f05fdb2c90da409f17d356f84be0de8e8cbb9
Damian Kasyan <damian.kasyan@hotmail.com>
2020-11-09 22:14:34 +0100

    Tofix

--- main.py
+++ main.py
@@ -12,6 +12,23 @@ carrier._is_mobile(number_type(phonenumbers.parse(number)))
 
 fake = Faker('pl_PL')
 
+#def label_length(funct):
+#    result = funct(self)
+#    print(result)
+
+
+        
+ 
+ #       logging.debug(result)
+
+
+#        a = len(name)
+#        b = len(surname)
+#        c = a + b 
+#    logging.debug(f'Długość imienia i nazwiska to {c}')
+#    return(c)
+
+
 
 #fake.profile()
 fakek_profil = fake.profile()
@@ -23,23 +40,20 @@ f_job = str.split(fakek_profil["job"])
 f_company = fake.bs()
 f_businesphone = fake.phone_number()
 
-
 class BaseContact:
     def __init__(self, name, surname, phone, email):
         self.name = name
         self.surname = surname
         self.phone = phone
         self.email = email
+    
     def contact(self):
         print(f'Wybieram numer {self.phone} i dzwonie do {self.name} {self.surname}')
     def __str__(self):
         return f'{self.name} {self.surname} {self.phone} {self.email}'
-    def label_length(self):
-        a = len(self.name)
-        b = len(self.surname)
-        c = a + b 
-        logging.debug(f'Długość imienia i nazwiska to {c}')
-        return(c)
+        
+
+
 
 
 class BusinessContact(BaseContact):
@@ -51,8 +65,8 @@ class BusinessContact(BaseContact):
 
     def contact(self):
         print(f'Wybieram numer {self.businesphone} i dzwonie do {self.name} {self.surname}')
-        logging.debug("Chose number")
-
+        logging.debug("Busines Contact contact method run:"  f'Wybieram numer {self.businesphone} i dzwonie do {self.name} {self.surname}' )
+#    @label_length
     def __str__(self):
         return f'{self.name} {self.surname} {self.phone} {self.email} {self.job} {self.company} {self.businesphone}'
 
@@ -83,12 +97,14 @@ print("################")
 kolega = BaseContact(name = f_name, surname = f_surname, phone = f_phone, email = f_email)
 kolega.contact()
 #print(kolega)
-kolega.label_length()
+
 
 print("################")
 wspolpracownik = BusinessContact(name = f_name, surname = f_surname, phone = f_phone, email = f_email, job = f_job, company = f_company, businesphone = f_businesphone)
 wspolpracownik.contact()
 #print(wspolpracownik)
+
+
 
 
 
