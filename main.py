1b0901bbc7a2b1be016e703aca184d3d2867599b
Damian Kasyan <damian.kasyan@hotmail.com>
2020-12-08 21:45:44 +0100

    last

--- main.py
+++ main.py
@@ -28,18 +28,6 @@ fake = Faker('pl_PL')
 #    logging.debug(f'Długość imienia i nazwiska to {c}')
 #    return(c)
 
-
-
-#fake.profile()
-fakek_profil = fake.profile()
-f_name = str.split(fakek_profil["name"])[0]
-f_surname = str.split(fakek_profil["name"])[1]
-f_phone = fake.phone_number()
-f_email = str.split(fakek_profil["mail"])[0]
-f_job = str.split(fakek_profil["job"])
-f_company = fake.bs()
-f_businesphone = fake.phone_number()
-
 class BaseContact:
     def __init__(self, name, surname, phone, email):
         self.name = name
@@ -53,28 +41,47 @@ class BaseContact:
         return f'{self.name} {self.surname} {self.phone} {self.email}'
         
 
+class BaseContact:
 
+    def __init__(self, name, surname, phone, email):
+        self.name = name
+        self.surname = surname
+        self.phone = phone
+        self.email = email
+
+
+    def contact(self):
+        print(f'Wybieram numer {self.phone} i dzwonie do {self.name} {self.surname}')
+
+    def __str__(self):
+        return f'{self.name} {self.surname} {self.phone} {self.email}'
+
+    @property
+    def label_length(self):
+        # TODO: czy potrzebujemy az tylu zmiennych?
+        a = len(self.name)
+        b = len(self.surname)
+        c = a + b
+        return c
 
 
 class BusinessContact(BaseContact):
     def __init__(self, job, company, businesphone, *args, **kwargs):
-        super().__init__( *args, **kwargs)
+        super().__init__(*args, **kwargs)
         self.job = job
         self.company = company
         self.businesphone = businesphone
 
     def contact(self):
         print(f'Wybieram numer {self.businesphone} i dzwonie do {self.name} {self.surname}')
-        logging.debug("Busines Contact contact method run:"  f'Wybieram numer {self.businesphone} i dzwonie do {self.name} {self.surname}' )
-#    @label_length
+        # logging.debug(
+        #     "Busines Contact contact method run:"  f'Wybieram numer {self.businesphone} i dzwonie do {self.name} {self.surname}')
+
+    #    @label_length
     def __str__(self):
         return f'{self.name} {self.surname} {self.phone} {self.email} {self.job} {self.company} {self.businesphone}'
 
-#Losowa wizytówka
-
-types = True
-amount = 1
-def create_contacts (types, amount):
+def create_contacts(business, amount):
     for i in range(amount):
         fakek_profil = fake.profile()
         fk_name = str.split(fakek_profil["name"])[0]
@@ -84,27 +91,17 @@ def create_contacts (types, amount):
         fk_job = str.split(fakek_profil["job"])
         fk_company = fake.bs()
         fk_businesphone = fake.phone_number()
-        if types == True:
-            private = BaseContact(name = fk_name, surname = fk_surname, phone = fk_phone, email = fk_email)
+        if business == False:
+            private = BaseContact(name=fk_name, surname=fk_surname, phone=fk_phone, email=fk_email)
             private.contact()
         else:
-            business = BusinessContact(name = fk_name, surname = fk_surname, phone = fk_phone, email = fk_email, job = fk_job, company = fk_company, businesphone = fk_businesphone)
+            business = BusinessContact(name=fk_name, surname=fk_surname, phone=fk_phone, email=fk_email, job=fk_job, company=fk_company, businesphone=fk_businesphone)
             business.contact()
 
-create_contacts(True,amount)
-
-print("################")      
-kolega = BaseContact(name = f_name, surname = f_surname, phone = f_phone, email = f_email)
-kolega.contact()
-#print(kolega)
 
 
-print("################")
-wspolpracownik = BusinessContact(name = f_name, surname = f_surname, phone = f_phone, email = f_email, job = f_job, company = f_company, businesphone = f_businesphone)
-wspolpracownik.contact()
-#print(wspolpracownik)
-
-
+create_contacts(False, 4)
+create_contacts(True, 2)
 
 
 
