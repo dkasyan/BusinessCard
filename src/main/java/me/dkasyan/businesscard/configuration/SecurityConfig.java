fa961bba8ef1ba396af45fea302d25c99a0da743
Gaba Bartnicka <bartnicka.g@gmail.com>
2022-02-05 23:52:24 +0100

    💄 nice login page

--- src/main/java/me/dkasyan/businesscard/configuration/SecurityConfig.java
+++ src/main/java/me/dkasyan/businesscard/configuration/SecurityConfig.java
@@ -1,6 +1,7 @@
 package me.dkasyan.businesscard.configuration;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import me.dkasyan.businesscard.configuration.security.CustomAuthenticationFilter;
 import me.dkasyan.businesscard.configuration.security.UserSkeleton;
 import org.jetbrains.annotations.NotNull;
@@ -10,13 +11,21 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
+@Slf4j
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserSkeleton userSkeleton;
@@ -28,11 +37,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.csrf().disable()
             .addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class)
             .authorizeRequests()
-            .antMatchers("/login*").permitAll()
+            .antMatchers("/login*", "/css/**", "/fonts/**", "/images/**", "/js/**", "/scss/**").permitAll()
             .anyRequest().authenticated().and()
-//            .formLogin()
-//            .and()
-        ;
+            .formLogin()
+            .loginPage("/login")
+            .failureUrl("/login?error=true").failureHandler(new AuthenticationFailureHandler() {
+                @Override
+                public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+                    log.error("Authentication failed!");
+                    response.sendRedirect("/login?error=true");
+                }
+            })
+            .defaultSuccessUrl("/", false).permitAll().and();
+
     }
 
     @Override
