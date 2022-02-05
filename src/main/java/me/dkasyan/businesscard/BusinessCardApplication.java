4379c4ae2145860c0291ae7a0ec2bc5e7f3ba759
Gaba Bartnicka <bartnicka.g@gmail.com>
2022-02-05 19:21:14 +0100

    🎉 setup java project

---
+++ src/main/java/me/dkasyan/businesscard/BusinessCardApplication.java
@@ -0,0 +1,13 @@
+package me.dkasyan.businesscard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessCardApplication.class, args);
+    }
+
+}
